b5cc5bc2cfb64a11e916dd2033694a83f5297a84
nathan206100 <nathansteward13@gmail.com>
2020-08-21 23:38:29 -0400

    fully functional "sticky" alt-tab on the rotary encoder

--- BDN9_custom/keymap.c
+++ BDN9_custom/keymap.c
@@ -14,33 +14,38 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
- /*	Nathan Steward 8/18/2020 Rev. 1.0.0
+ /*	Nathan Steward 8/21/2020 Rev. 1.1.0
 
  	TODO:
- 		- TBA
+ 		DONE - blank 3-layer setup
+        DONE - implement "sticky" alt tab on rotary encoder
 
  	CHANGELOG:
  		- Rev 1.0.0
-         - initial setup for macropad
-         - needs planning still
-
+            - initial setup for macropad
+            - needs planning still
+        - Rev 1.1.0
+            - full implementation of working alt-tab/alt-sft-Tab
+                - exits tabbed screen on rotary button press
  */
 
 #include QMK_KEYBOARD_H
 
-static bool alt_lock_bit = false;
+bool alt_lock_bit = false;
 
-#define _BASE 0u
-#define _FN_ONE 1u
-#define _FN_TWO 2u
+enum layers {
+    _BASE   = 0,
+    _FN_ONE = 1,
+    _FN_TWO = 2
+};
 
 enum custom_keycodes {
     ALT_LOCK = SAFE_RANGE
 };
 
-#define FN_BASE DF(_BASE)
-#define FN_ONE DF(_FN_ONE)
-#define FN_TWO DF(_FN_TWO)
+#define FN_BASE TO(_BASE)
+#define FN_ONE  TO(_FN_ONE)
+#define FN_TWO  TO(_FN_TWO)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
@@ -50,51 +55,56 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         | Left              | Down | Right              |
      */
     [_BASE] = LAYOUT(
-        KC_MUTE, FN_ONE,  FN_TWO,
-        XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX
+        KC_MUTE, FN_ONE,  FN_TWO,  \
+        XXXXXXX, XXXXXXX, XXXXXXX, \
+        XXXXXXX, XXXXXXX, XXXXXXX  \
     ),
     [_FN_ONE] = LAYOUT(
-        ALT_LOCK,FN_BASE, FN_TWO,
-        XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX
+        ALT_LOCK,FN_BASE, FN_TWO,  \
+        XXXXXXX, XXXXXXX, XXXXXXX, \
+        XXXXXXX, XXXXXXX, XXXXXXX  \
     ),
     [_FN_TWO] = LAYOUT(
-        KC_MUTE, FN_ONE,  FN_BASE,
-        XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX
-    ),
+        XXXXXXX, FN_ONE,  FN_BASE, \
+        XXXXXXX, XXXXXXX, XXXXXXX, \
+        XXXXXXX, XXXXXXX, XXXXXXX  \
+    )
 };
 
 void encoder_update_user(uint8_t index, bool clockwise) {
-    if (layer_state == _BASE) {
-        if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
-            tap_code(KC_VOLD);
-        }
-    }
-    else if (layer_state == _FN_ONE){
-        if (clockwise) {
-            tap_code16(KC_TAB);
-        } else {
-            tap_code16(LSFT(KC_TAB));
-        }
+    switch(biton32(layer_state)){
+        case _BASE:
+            if (clockwise) {
+                tap_code(KC_VOLD);
+                break;
+            }
+            else {
+                tap_code(KC_VOLU);
+                break;
+            }
+        case _FN_ONE:
+            if (clockwise) {
+                register_code(KC_LALT);
+                register_code(KC_LSFT);
+                tap_code(KC_TAB);
+                unregister_code(KC_LSFT);
+                break;
+            } else {
+                register_code(KC_LALT);
+                tap_code(KC_TAB);
+                break;
+            }
+
     }
 }
 // "KEY_LOCK_ENABLE = yes" must be set in rules.mk
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case ALT_LOCK:
-            if(alt_lock_bit == false) {
-                tap_code16(KC_LOCK);
-                tap_code16(KC_LALT);
-                alt_lock_bit = true;
+            if(record->event.pressed){
+                unregister_code(KC_LALT);
+                break;
             }
-            else {
-                tap_code16(KC_LALT);
-                alt_lock_bit = false;
-            }
-  }
-  return true;
+    }
+    return true;
 }
